ce3cb09def7f44b01462e67ad2b32ba19e07e0ec
Saul Fernandez <sarony11@gmail.com>
2023-06-15 23:45:23 +0200

    feat: import libraries and setup some variables

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import random
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+SNAKE_SIZE = 20
+FOOD_SIZE = 20
+SNAKE_COLOR = (0, 255, 0)  # Green
+FOOD_COLOR = (255, 0, 0)  # Red
+BACKGROUND_COLOR = (0, 0, 0)  # Black
